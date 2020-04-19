75ac17075b4e3ab5040fe4fcaf753a5bd568aecd
USER <USER@salon.local>
2020-04-19 11:03:05 +0530

    change 2

--- User/src/com/test/Test.java
+++ User/src/com/test/Test.java
@@ -1,9 +1,13 @@
 package com.test;
 
+import com.User1;
+
 public class Test {
 
 	
 	public static void main(String[] args) {
 		System.out.println("nagaraj");
+		User1 u = new User1();
+		System.out.println("change 2");
 	}
 }
